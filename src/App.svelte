1212c07ddf3935e8bb80bf86e9865c09d163dd8d
abidurr <arahim@rapptrlabs.com>
2021-11-21 17:43:32 -0500

    add player

--- src/App.svelte
+++ src/App.svelte
@@ -1,16 +1,24 @@
 <script>
   import Navbar from "./Navbar.svelte";
   import Player from "./Player.svelte";
+  import AddPlayer from "./AddPlayer.svelte";
 
   let players = [
     { name: "John Doe", points: 22 },
     { name: "Jane Doe", points: 12 },
     { name: "John Titor", points: 15 },
   ];
+
+  const addPlayer = (e) => {
+    const newPlayerData = e.detail;
+    // States are immutable so you can't push it
+    players = [...players, newPlayerData];
+  };
 </script>
 
 <main>
-	<Navbar />
+  <Navbar />
+  <AddPlayer on:addplayer={addPlayer} />
   {#if players.length === 0}
     <p>No players</p>
   {:else}
